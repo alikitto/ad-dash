eb3c6a4bd5ecc0667731fc0715873d63c090ed58
alikitto <aleskerov.alekper@gmail.com>
2025-08-21 17:52:42 +0400

    Update clientAvatars.js

--- frontend/src/variables/clientAvatars.js
+++ frontend/src/variables/clientAvatars.js
@@ -2,6 +2,6 @@
 export const CLIENT_AVATARS = {
   // ключом удобнее делать account_id (если есть), иначе account_name
   "Ahad Nazim": "https://video.karal.az/avatars/ahadnazim.jpg",
-  "My Business Manager": "https://cdn.example.com/logos/brand-b.jpg",
-  "Other Account Name": "https://cdn.example.com/logos/brand-c.png",
+  "Tural Multi": "https://video.karal.az/avatars/gf-diamonds.png",
+  "Elnur Agayev": "https://video.karal.az/avatars/gf.jpg",
 };
