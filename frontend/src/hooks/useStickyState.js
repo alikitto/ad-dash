fb27fd58e8e411bcdb61e75bb9c8d4832b55c928
alikitto <aleskerov.alekper@gmail.com>
2025-08-28 01:11:35 +0400

    Create useStickyState.js

---
+++ frontend/src/hooks/useStickyState.js
@@ -0,0 +1,20 @@
+// src/hooks/useStickyState.js
+
+import { useState, useEffect } from "react";
+
+export function useStickyState(defaultValue, key) {
+  const [value, setValue] = useState(() => {
+    try {
+      const stickyValue = window.localStorage.getItem(key);
+      return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
+    } catch {
+      return defaultValue;
+    }
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
