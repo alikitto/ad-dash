8cd04a5b81a8171f1c73ea8611c29cb7cf5d0bf1
alikitto <aleskerov.alekper@gmail.com>
2025-08-28 03:04:19 +0400

    Update Auth.js

--- frontend/src/layouts/Auth.js
+++ frontend/src/layouts/Auth.js
@@ -1,10 +1,36 @@
-// src/layouts/Auth.js
+/*!
 
+=========================================================
+* Vision UI Free Chakra - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/vision-ui-free-chakra
+* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
+* Licensed under MIT (https://github.com/creativetimofficial/vision-ui-free-chakra/blob/master LICENSE.md)
+
+* Design and Coded by Simmmple & Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import routes from "routes.js";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import theme from "theme/themeAuth.js";
 
-export default function Pages(props) {
+
+export default function AuthLayout(props) {
+  const wrapper = React.createRef();
+
+  React.useEffect(() => {
+    document.body.style.overflow = "unset";
+    // Specify how to clean up after this effect:
+    return function cleanup() {};
+  });
+  
   const getRoutes = (routes) => {
     return routes.map((prop, key) => {
       if (prop.collapse) {
@@ -28,11 +54,15 @@ export default function Pages(props) {
   };
 
   return (
-    <div> {/* Простая обёртка, без Sidebar и Navbar */}
-      <Switch>
-        {getRoutes(routes)}
-        <Redirect from='/auth' to='/auth/signin' />
-      </Switch>
-    </div>
+    <ChakraProvider theme={theme} resetCss={false} w='100%'>
+      <Box w='100%'>
+        <Box ref={wrapper} w='100%'>
+          <Switch>
+            {getRoutes(routes)}
+            <Redirect from='/auth' to='/auth/signin' />
+          </Switch>
+        </Box>
+      </Box>
+    </ChakraProvider>
   );
 }
